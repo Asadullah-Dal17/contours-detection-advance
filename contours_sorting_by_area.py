d2421716feedd137b423ea02bec8cdfbce9a4cf1
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-11-26 17:46:28 +0500

    sorted contours by the area

--- contours_sorting_by_area.py
+++ contours_sorting_by_area.py
@@ -1,25 +1,36 @@
 import cv2 as cv 
 import numpy as np
-img = cv.imread('./sample-image3.png')
-blur_image = cv.GaussianBlur(img, (3,3),0)
-edges_img = cv.Canny(blur_image, 100, 150)
+def areaFinder(contours):
+    areas = []
+    for c in contours:
+        a =cv.contourArea(c)
+        areas.append(a)
+    return areas
+        
+img = cv.imread('./Images/sample.jpg')
+# blur_image = cv.GaussianBlur(img, (3,3),0)
+edges_img = cv.Canny(img, 100, 150)
 cv.imshow('edges', edges_img)
-contours, h = cv.findContours(edges_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+contours, h = cv.findContours(edges_img, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 max_area =0
-for c in contours:
-    area = cv.contourArea(c)
-    perimeter = cv.arcLength(c, True)
-    # print(perimeter)
-    epsilon = 0.05*cv.arcLength(c,True)
-    approx = cv.approxPolyDP(c,epsilon,True)
-    print(f'appx: {approx}, perimeter: {perimeter}')
-    # print(area)
-    M = cv.moments(c)
-    # print(center_point)
-    cx = int(M['m10']/M['m00'])
-    cy = int(M['m01']/M['m00'])
-    cv.putText(img, f'{area} A, {len(approx)} APX', (cx-15, cy), cv.FONT_HERSHEY_PLAIN, 0.7, (0,255,244), 1, cv.LINE_AA)
-    # # cv.circle(img,(cx, cy), 2, 255, -1)
+sorted_contours = sorted(contours, key=cv.contourArea,reverse=False )
+print(areaFinder(contours))
+print(areaFinder(sorted_contours))
+
+# for c in contours:
+#     area = cv.contourArea(c)
+#     perimeter = cv.arcLength(c, True)
+#     # print(perimeter)
+#     epsilon = 0.05*cv.arcLength(c,True)
+#     approx = cv.approxPolyDP(c,epsilon,True)
+#     print(f'appx: {approx}, perimeter: {perimeter}')
+#     # print(area)
+#     M = cv.moments(c)
+#     # print(center_point)
+#     cx = int(M['m10']/M['m00'])
+#     cy = int(M['m01']/M['m00'])
+#     cv.putText(img, f'{area} A, {len(approx)} APX', (cx-15, cy), cv.FONT_HERSHEY_PLAIN, 0.7, (0,255,244), 1, cv.LINE_AA)
+#     # # cv.circle(img,(cx, cy), 2, 255, -1)
     
 cv.drawContours(img, contours, -1, 255, 2)
 # cv.drawContours(img, big_contours,-1, (0,255,0), 7)
