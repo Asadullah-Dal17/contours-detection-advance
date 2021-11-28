25ada352472529b5440151855cfeb751b9bdf3d8
asadullah-dal-17 <asadullah.dal17@gmail.com>
2021-11-28 18:02:02 +0500

    get the different bounding box

--- code/sorting_by_location.py
+++ code/sorting_by_location.py
@@ -1,7 +1,7 @@
 import cv2 as cv 
 import numpy as np
 
-img_path = '../Images/sample.jpg'
+img_path = '../Images/sample-image3.png'
 #  reading the images 
 img = cv.imread(img_path)
 
@@ -9,6 +9,20 @@ denoised_img = cv.GaussianBlur(img, (3,3), 10)
 
 edges_img = cv.Canny(denoised_img, 100,200)
 dilate_img = cv.dilate(edges_img, (3,3), iterations=3)
+contours, h = cv.findContours(dilate_img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+for cnt  in contours:
+    bbox = cv.boundingRect(cnt)
+    # print(type(bbox))
+    cv.rectangle(img, bbox, color=(0,0, 0), thickness=3)
+    rect = cv.minAreaRect(cnt)
+    # print(rect)
+    box = cv.boxPoints(rect)
+    # print(box)
+    box = np.int0(box)
+    cv.drawContours(img, [box], 0, (0,255, 0), 2)
+    # cv.rectangle(img, box, color=(0, 255, 0),thickness=2 )
+    cv.imshow('img', img)
+    cv.waitKey(0)
 
 cv.imshow('dilated_img', dilate_img)
 # cv.imshow('img', img)
